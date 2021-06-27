5fcf08bdd96cddd41c1dd59194403935287c8b42
Taha Yıldırım <thyldrm23@gmail.com>
2021-06-27 16:11:31 +0300

    custom middlewares

--- misfit/routes/pageRoute.js
+++ misfit/routes/pageRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const pageController = require("../controllers/pageController");
+const redirectMidlleware = require("../middlewares/redirectMiddleware")
 
 const router = express.Router();
 
@@ -8,8 +9,8 @@ router.route("/index").get(pageController.getIndexPage);
 router.route("/about").get(pageController.getAboutPage);
 router.route("/trainer").get(pageController.getTrainerPage);
 router.route("/contact").get(pageController.getContactPage);
-router.route("/register").get(pageController.getRegisterPage);
-router.route("/login").get(pageController.getLoginPage);
+router.route("/register").get(redirectMidlleware, pageController.getRegisterPage);
+router.route("/login").get(redirectMidlleware, pageController.getLoginPage);
 
 
 module.exports=router;
